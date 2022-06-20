154dbc7dde6921c8fec66faf1e7c6c2251bb85dc
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-20 11:18:36 +0530

    Create armstrong.cpp

---
+++ armstrong.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int sum = 0;
+    while(n>0){
+        int lastdigit = n%10;
+        sum = sum + pow(lastdigit,3);
+        n= n/10;
+    }
+    cout<<sum;
+    
+}
