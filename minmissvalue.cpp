7148ccae8d3df4d0e1a8890caf487410f7cc9d2f
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-22 16:44:10 +0530

    Create minmissvalue.cpp

---
+++ minmissvalue.cpp
@@ -0,0 +1,37 @@
+// find the minimum number in array those are missing
+#include<iostream>
+#include<cmath>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    const int N = 1e6+2;
+    bool check[N];
+    for(int i=0; i<N; i++){
+        check[i]= false;
+    }
+
+    for(int i=0; i<n; i++){
+        if(arr[i]>=0){
+            check[arr[i]] = true;
+        }    
+    }
+
+    for(int i=0; i<n; i++){
+        if(check[i]==false){
+            cout<<i<<endl;
+            break;
+        }
+    }
+
+
+    return 0;
+}
