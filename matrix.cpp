8b19c49e3de5463cda5e58b7a8b8736fd3fdc5ef
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-24 10:39:51 +0530

    Create matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,28 @@
+// create matrix and print
+#include<iostream>
+#include<cmath>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int n;
+    int m;
+    cin>>n>>m;
+    int arr[n][m];
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
